9b135419adf91e40dab5af60bae3e9ea24545597
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2024-05-22 03:01:01 +0100

    movie fetching and displaying and also refreshing on pull of the screen

--- Components/Home.jsx
+++ Components/Home.jsx
@@ -1,44 +1,101 @@
-import { ActivityIndicator, Button, FlatList, Image, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { ActivityIndicator, Button, FlatList, Image, RefreshControl, RefreshControlComponent, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const Home = () => {
   const [isLoading, setLoading] = useState(false)
   const [data, setData] = useState([])
-  // let url = "https://api.github.com/users"
+  const [refreshing, setRefreshing] = useState(false)
   const apiKey = '86ea365e1002d9d79178416518dded40'
   const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`
 
+
+  
   useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = () => {
     setLoading(true)
     axios.get(url)
-    .then((res)=>{
-      console.log(res.data)
-      setLoading(false)
-      setData(res.data.results)
-    }) 
-  }, [])
-  
- 
-  const renderHorizontalList = () => (
-    <FlatList
+      .then((res) => {
+        console.log(res.data)
+        setLoading(false)
+        setData(res.data.results)
+      })
+      .catch((error) => {
+        console.error(error)
+        setLoading(false)
+      })
+  }
+
+
+  const onRefresh = () => {
+    setRefreshing(true)
+    axios.get(url)
+      .then((res) => {
+        setData(res.data.results)
+        setRefreshing(false)
+      })
+      .catch((error) => {
+        console.error(error)
+        setRefreshing(false)
+      })
+  }
+
+  const shuffleArray = (array) => {
+    let shuffledArray = array.slice()
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1))
+      ;[shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]]
+    }
+    return shuffledArray
+  }
+
+  return (
+    <View style={styles.container}>
+      {isLoading ? (
+        <ActivityIndicator size='large' color='#fff' style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }} />
+      ) : (
+        <ScrollView
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+        >
+          <Text style={styles.headertext}>Popular Movies</Text>
+          <FlatList
+            data={shuffleArray(data)}
+            horizontal={true}
+            renderItem={({ item }) => (
+              <View style={styles.list}>
+                <Image source={{ uri: `https://image.tmdb.org/t/p/w500${item.poster_path}` }} style={styles.image} />
+                <Text style={styles.text}>{item.title}</Text>
+              </View>
+            )}
+            keyExtractor={(item) => item.id.toString()}
+            showsHorizontalScrollIndicator={false}
+
+          />
+
+          <Text style={styles.headertext}>Top Rated Movies</Text>
+          <FlatList
       data={data}
-      horizontal={true}
+      horizontal={shuffleArray(true)}
       renderItem={({ item }) => (
-        <View style={styles.list}>
+        <View style={styles.listGrid}>
           <Image source={{ uri: `https://image.tmdb.org/t/p/w500${item.poster_path}` }} style={styles.image} />
           <Text style={styles.text}>{item.title}</Text>
         </View>
       )}
       keyExtractor={(item) => item.id.toString()}
-      showsHorizontalScrollIndicator={false}
-      contentContainerStyle={styles.contentContainer}
-    />
-  )
+      showsVerticalScrollIndicator={false}
 
-  const renderVerticalGrid = () => (
-    <FlatList
-      data={data}
+
+    />
+
+          <Text style={styles.headertext}>Upcoming Movies</Text>
+          <FlatList
+      data={shuffleArray(data)}
       horizontal={true}
       renderItem={({ item }) => (
         <View style={styles.listGrid}>
@@ -48,24 +105,9 @@ const Home = () => {
       )}
       keyExtractor={(item) => item.id.toString()}
       showsVerticalScrollIndicator={false}
-      contentContainerStyle={styles.contentContainerGrid}
+
+
     />
-  )
-
-  return (
-    <View style={styles.container}>
-      {isLoading ? (
-        <ActivityIndicator size='large' color='#fff' />
-      ) : (
-        <ScrollView>
-          <Text style={styles.headertext}>Popular Movies</Text>
-          {renderHorizontalList()}
-
-          <Text style={styles.headertext}>Top Rated Movies</Text>
-          {renderVerticalGrid()}
-
-          <Text style={styles.headertext}>Upcoming Movies</Text>
-          {renderHorizontalList()}
         </ScrollView>
       )}
     </View>
@@ -74,35 +116,28 @@ const Home = () => {
 
 export default Home
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#000',
-//     paddingVertical: 20,
-//   },
-//   contentContainer: {
-//     paddingLeft: 10,}
-// })
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#000',
-  
+
   },
   text: {
     color: '#fff',
     width: 120,
-    fontFamily:'sans-serif',
+    fontFamily: 'sans-serif',
   },
 
   headertext: {
-    color: '#fff',
-    fontWeight:'bold',
-    fontSize: 30,
-    
+    color: 'goldenrod',
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginVertical: 10,
+
 
   },
-  list:{
+  list: {
     alignItems: 'center',
     marginRight: 10,
   },
@@ -111,5 +146,9 @@ const styles = StyleSheet.create({
     height: 200,
     borderRadius: 10,
   },
- 
+
+  listGrid: {
+    alignItems: 'center',
+    margin: 10,
+  },
 })
