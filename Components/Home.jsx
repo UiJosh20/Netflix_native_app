183f5298df4837cb7c086c5d6d5b73f99ccc53a2
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2024-05-22 01:31:14 +0100

    added a flatlist component in my home page

--- Components/Home.jsx
+++ Components/Home.jsx
@@ -1,13 +1,43 @@
-import { ScrollView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { ActivityIndicator, Button, FlatList, Image, ScrollView, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const Home = () => {
+  const [isLoading, setLoading] = useState(false)
+  const [data, setData] = useState([])
+  let url = "https://api.github.com/users"
+
+  const loader = () => {
+    setLoading(true)
+    axios.get(url)
+    .then((res)=>{
+      console.log(res.data)
+      setLoading(false)
+      setData(res.data)
+    })
+  }
+
+
   return (
     <View style={styles.container}>
-      <ScrollView>
-       
+      
+        <Button title='tap me' onPress={loader}/>   
+        <ActivityIndicator size='large' animating={isLoading} ></ActivityIndicator>
+        <FlatList
+        data={data}
+        
+        renderItem={({item}) => (
+          <View>
+            <Text style={{color:'#fff'}}>{item.login}</Text>
+            <Image source={{ uri: `https://robohash.org/${item.login}` }} width={70} height={70}/>
+          </View>
+        )}
+        keyExtractor={(item)=>item.id}
+        >
 
-      </ScrollView>
+        </FlatList>
+     
+      
     </View>
   )
 }
@@ -18,5 +48,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#000',
-  }
+  
+  },
+ 
 })
