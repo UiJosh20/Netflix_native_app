40182fbbce1d33b62d39ab1fa453063c64c40a11
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2024-05-27 14:23:20 +0100

    added tab icons and now plans on adding header icons

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,18 +1,55 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Tabs } from 'expo-router'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
 
 const Tablayout = () => {
   return (
     <Tabs
-    screenOptions={{}}
+      screenOptions={({ route }) => ({
+        tabBarStyle: {
+          backgroundColor: 'black',
+          borderBlockColor:'black',
+        },
+        tabBarLabelStyle: {
+          color: '#fff',
+        },
+        tabBarActiveTintColor: '#fff',
+        tabBarInactiveTintColor: 'gray',
+        headerStyle: {
+          backgroundColor: 'black',
+        },
+        headerTintColor: '#fff',
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'index') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'hot') { 
+            iconName = focused ? 'flame' : 'flame-outline';
+          }else if (route.name === 'setting') { 
+            iconName = focused ? 'settings' : 'settings-outline';
+          }
+
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
     >
-        <Tabs.Screen name='index'
-        options={{title:"Home"}}
-        />
+      <Tabs.Screen
+        name='index'
+        options={{ title: "Home" }}
+      />
+      <Tabs.Screen
+        name='hot'
+        options={{ title: "Trending" }} 
+      />
+      <Tabs.Screen
+        name='setting'
+        options={{ title: "My Netflix" }} 
+      />
     </Tabs>
-  )
+  );
 }
 
-export default Tablayout
-
+export default Tablayout;
---
+++ app/(tabs)/hot.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const hot = () => {
+  return (
+    <View>
+      <Text>Hot</Text>
+    </View>
+  )
+}
+
+export default hot
+
+const styles = StyleSheet.create({})
