c3e30352c756171f05982227b97e99db23764f6d
lonexil <adetonalonex@gmail.com>
2024-06-02 04:02:47 -0500

    lost but found

--- Components/Home.jsx
+++ Components/Home.jsx
@@ -1,90 +1,159 @@
-import { ActivityIndicator, Button, FlatList, Image, Platform, RefreshControl, RefreshControlComponent, ScrollView, StatusBar, StyleSheet, Text, View } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-
+import {
+  ActivityIndicator,
+  Button,
+  FlatList,
+  Image,
+  Platform,
+  RefreshControl,
+  RefreshControlComponent,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home = () => {
-  const [isLoading, setLoading] = useState(false)
-  const [data, setData] = useState([])
-  const [refreshing, setRefreshing] = useState(false)
-  const apiKey = process.env.TMDB_API_KEY
-  const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`
-
-
+  const [isLoading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
+  const apiKey = process.env.API_KEY;
+  const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
 
   useEffect(() => {
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   const fetchData = () => {
-    setLoading(true)
-    axios.get(url)
+    setLoading(true);
+    axios
+      .get(url)
       .then((res) => {
         setTimeout(() => {
-          setLoading(false)
+          setLoading(false);
         }, 1000);
-        setData(res.data.results)
+        setData(res.data.results);
       })
       .catch((error) => {
-        console.error(error)
-        setLoading(false)
-      })
-  }
-
+        console.error(error);
+        setLoading(false);
+      });
+  };
 
   const onRefresh = () => {
-    setRefreshing(true)
-    axios.get(url)
+    setRefreshing(true);
+    axios
+      .get(url)
       .then((res) => {
-        setData(res.data.results)
-        setRefreshing(false)
+        setData(res.data.results);
+        setRefreshing(false);
       })
       .catch((error) => {
-        console.error(error)
-        setRefreshing(false)
-      })
-  }
+        console.error(error);
+        setRefreshing(false);
+      });
+  };
 
   const shuffleArray = (array) => {
-    let shuffledArray = array.slice()
+    let shuffledArray = array.slice();
     for (let i = shuffledArray.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1))
-      ;[shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]]
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [
+        shuffledArray[j],
+        shuffledArray[i],
+      ];
     }
-    return shuffledArray
-  }
+    return shuffledArray;
+  };
 
   return (
     <View style={styles.container}>
-      
       {isLoading ? (
-        <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
-          <Image source={require('../assets/anim.gif')}  />
+        <View
+          style={{ justifyContent: "center", alignItems: "center", flex: 1 }}
+        >
+          <Image source={require("../assets/anim.gif")} />
         </View>
-        // <ActivityIndicator size='large' color='#fff'  />
       ) : (
         <ScrollView
           refreshControl={
             <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
           }
         >
-          <Image 
-              source={{ uri: `https://image.tmdb.org/t/p/w500${data[10]?.poster_path}` }} 
-              style={styles.singlePhoto} 
+          <View>
+            <Image
+              source={{
+                uri: `https://image.tmdb.org/t/p/w500${data[10]?.poster_path}`,
+              }}
+              style={styles.singlePhoto}
             />
+            <Text style={styles.textposition}>{data[10]?.title}</Text>
+            <View
+              style={{
+                flexDirection: "row-reverse",
+                justifyContent: "space-between",
+                alignItems: "center",
+                paddingHorizontal: 30,
+                position: "absolute",
+                top: 250,
+                width: "100%",
+              }}
+            >
+              <TouchableOpacity
+                style={{
+                  backgroundColor: "#E50914",
+                  width: 100,
+                  justifyContent: "center",
+                  alignItems: "center",
+                  padding: 10,
+                  borderRadius: 10,
+                }}
+              >
+                <Text
+                  style={{ color: "white", fontSize: 16, fontWeight: "bold" }}
+                >
+                  Play
+                </Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={{
+                  backgroundColor: "grey",
+                  width: 120,
+                  alignItems: "center",
+                  padding: 10,
+                  borderRadius: 10,
+                }}
+              >
+                <Text
+                  style={{ color: "white", fontSize: 16, fontWeight: "bold" }}
+                >
+                  Watch later
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
           <Text style={styles.headertext}>Popular Movies</Text>
+
           <FlatList
             data={shuffleArray(data)}
             horizontal={true}
             renderItem={({ item }) => (
               <View style={styles.list}>
-                <Image source={{ uri: `https://image.tmdb.org/t/p/w500${item.poster_path}` }} style={styles.image} />
+                <Image
+                  source={{
+                    uri: `https://image.tmdb.org/t/p/w500${item.poster_path}`,
+                  }}
+                  style={styles.image}
+                />
                 <Text style={styles.text}>{item.title}</Text>
               </View>
             )}
             keyExtractor={(item) => item.id.toString()}
             showsHorizontalScrollIndicator={false}
-
           />
 
           <Text style={styles.headertext}>Top Rated Movies</Text>
@@ -93,14 +162,17 @@ const Home = () => {
             horizontal={true}
             renderItem={({ item }) => (
               <View style={styles.listGrid}>
-                <Image source={{ uri: `https://image.tmdb.org/t/p/w500${item.poster_path}` }} style={styles.image} />
+                <Image
+                  source={{
+                    uri: `https://image.tmdb.org/t/p/w500${item.poster_path}`,
+                  }}
+                  style={styles.image}
+                />
                 <Text style={styles.text}>{item.title}</Text>
               </View>
             )}
             keyExtractor={(item) => item.id.toString()}
             showsVerticalScrollIndicator={false}
-
-
           />
 
           <Text style={styles.headertext}>Upcoming Movies</Text>
@@ -109,49 +181,57 @@ const Home = () => {
             horizontal={true}
             renderItem={({ item }) => (
               <View style={styles.listGrid}>
-                <Image source={{ uri: `https://image.tmdb.org/t/p/w500${item.poster_path}` }} style={styles.image} />
+                <Image
+                  source={{
+                    uri: `https://image.tmdb.org/t/p/w500${item.poster_path}`,
+                  }}
+                  style={styles.image}
+                />
                 <Text style={styles.text}>{item.title}</Text>
               </View>
             )}
             keyExtractor={(item) => item.id.toString()}
             showsVerticalScrollIndicator={false}
-
-
           />
         </ScrollView>
       )}
     </View>
-  )
-}
+  );
+};
 
-export default Home
-
+export default Home;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
-    paddingTop: Platform.OS==='android'? StatusBar.currentHeight: 0,
-
-
+    backgroundColor: "#000",
   },
- 
+
   text: {
-    color: '#fff',
+    color: "#fff",
     width: 120,
-    fontFamily: 'sans-serif',
+    fontFamily: "sans-serif",
+  },
+
+  textposition: {
+    color: "#fff",
+    fontSize: 27,
+    fontWeight: "bold",
+    fontFamily: "sans-serif",
+    textAlign: "center",
+    position: "relative",
+    top: -330,
+    // left: 50,
   },
 
   headertext: {
-    color: 'goldenrod',
-    fontWeight: 'bold',
+    color: "goldenrod",
+    fontWeight: "bold",
     fontSize: 20,
     marginVertical: 20,
-
-
   },
   list: {
-    alignItems: 'center',
+    alignItems: "center",
     marginRight: 10,
   },
   image: {
@@ -161,11 +241,12 @@ const styles = StyleSheet.create({
   },
 
   listGrid: {
-    alignItems: 'center',
+    alignItems: "center",
     margin: 10,
-    marginVertical:20,
+    marginVertical: 20,
   },
-  singlePhoto:{
-    height:400,
-}
-})
+  singlePhoto: {
+    height: 500,
+    opacity: 0.4,
+  },
+});
